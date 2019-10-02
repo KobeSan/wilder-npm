24686ebad7f8cf703ca870764b9f831242c83156
KobeSan <baptiste.gislot@gmail.com>
2019-10-02 10:40:48 +0200

    Finalisation du projet

---
+++ index.js
@@ -0,0 +1,6 @@
+const chalk = require('chalk');
+
+console.log(chalk.bgBlue.black('Elise'));
+console.log(chalk.bgMagenta.black('Martin'));
+console.log(chalk.bgGreen.black('Aurelia'));
+console.log(chalk.bgBlack.white('Titouan'));
